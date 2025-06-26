25fcf479d4ca46582ef84ccab5445d5cf47984ea
Toshiba2925 <aqifashiba25@gmail.com>
2025-06-26 11:10:24 +0700

    membuat void post order

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -123,5 +123,20 @@ public:
         preorder(ptr->rightchild);
     }
 
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+    
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; //parent
+    }
+
     
 }
