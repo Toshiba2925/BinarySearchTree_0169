07bd8a4cda03637cc17fd3f2807ca86a5e48d19d
Toshiba2925 <aqifashiba25@gmail.com>
2025-06-26 11:09:39 +0700

    membuat void pre order

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -108,5 +108,20 @@ public:
         inorder(ptr->rightchild);
     }
 
+    void preorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        cout << ptr->info << " "; //parent    
+        preorder(ptr->leftchild);
+        preorder(ptr->rightchild);
+    }
+
     
 }
