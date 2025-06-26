f5cfdd3d3b1b5c4bb4d8b1b77e3dbd995edc6535
Toshiba2925 <aqifashiba25@gmail.com>
2025-06-26 10:56:46 +0700

    step 6 void insert

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -57,6 +57,16 @@ public:
             return;
         }
 
+        // Step 6: If the value in the data field of new Node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a: Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+            // 6b: Exit
+            return;
+        }
+
         
     }
 }
